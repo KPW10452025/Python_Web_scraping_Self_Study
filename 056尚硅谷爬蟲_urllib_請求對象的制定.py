2d3c485fe44d0b1b2ce498fb0f3d80155598bf8e
PoWei <10452025@mail.ntou.edu.tw>
2021-10-16 15:32:52 +0800

    運用「請求對象訂製」將headers放入其中，仍出現TypeError

--- 056尚硅谷爬蟲_urllib_請求對象的制定.py
+++ 056尚硅谷爬蟲_urllib_請求對象的制定.py
@@ -11,9 +11,18 @@ headers = {
     'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
 }
 
-response = urllib.request.urlopen(url, headers)
+# 藉由 command + 點擊 urlopen 看其定義。
+# Open the URL url, which can be either a string or a Request object.
+# 因為 urlopen 方法中不能存儲字典，所以 headers 不能傳遞進去。
+# 但是 urlopen 方法可以存處 Request
+
+# 故使用「請求對象訂製」：
+request = urllib.request.Request(url, headers)
+
+# response = urllib.request.urlopen(url, headers)
+response = urllib.request.urlopen(request)
+# 把設定好的 request 放入上式，替換掉 url, headers
 
 content = response.read().decode('utf-8')
 
 print(content)
-# TypeError: can't concat str to bytes
