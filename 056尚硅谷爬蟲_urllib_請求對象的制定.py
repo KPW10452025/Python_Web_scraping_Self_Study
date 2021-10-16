0f5e022ec9d22b564f3821273582ad74e25b4e59
PoWei <10452025@mail.ntou.edu.tw>
2021-10-16 15:21:13 +0800

    設定headers仍出現TypeError

--- 056尚硅谷爬蟲_urllib_請求對象的制定.py
+++ 056尚硅谷爬蟲_urllib_請求對象的制定.py
@@ -2,27 +2,18 @@ import urllib.request
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# url 的組成
-
-# 協議          主機              端口號
-
-# http/https    www.google.com   80/443
-
-# 常見端口號
-# http      80
-# https     443
-# MySQL     3306
-# oracle    1521
-# redis     6379
-# mongodb   27017
-
 url = 'https://www.gamer.com.tw/'
 
-response = urllib.request.urlopen(url)
+# 尋找網頁 User Agent
+# user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36
+
+headers = {
+    'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
+}
+
+response = urllib.request.urlopen(url, headers)
 
 content = response.read().decode('utf-8')
 
 print(content)
-
-# 被反爬蟲
-# urllib.error.HTTPError: HTTP Error 403: Forbidden
+# TypeError: can't concat str to bytes
