ddac6fb6866ecf3711341d3b869d73bb7f7cdff8
PoWei <10452025@mail.ntou.edu.tw>
2021-10-16 12:48:29 +0800

    第六個方法：getheaders()

--- 054尚硅谷爬蟲_urllib_一個類型和六個方法.py
+++ 054尚硅谷爬蟲_urllib_一個類型和六個方法.py
@@ -44,6 +44,14 @@ print(type(response))
 # 返回 200 只的是網路順暢無錯誤。
 
 # 第五個方法：geturl()
-print(response.geturl())
+# print(response.geturl())
 # https://www.baidu.com/
 # 返回「網址」
+
+# 第六個方法：getheaders()
+print(response.getheaders())
+# [('Bdpagetype', '1'), ('Bdqid', '0x8bcc24b70000041c'), ('Cache-Control', 'private'), ('Content-Type', 'text/html;charset=utf-8'), ('Date', 'Sat, 16 Oct 2021 04:45:28 GMT'), ('Expires', 'Sat, 16 Oct 2021 04:45:28 GMT'), ('P3p', 'CP=" OTI DSP COR IVA OUR IND COM "'), ('P3p', 'CP=" OTI DSP COR IVA OUR IND COM "'), ('Server', 'BWS/1.1'), ('Set-Cookie', 'BAIDUID=785ED912AB14622AD33E640EE0D87133:FG=1; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com'), ('Set-Cookie', 'BIDUPSID=785ED912AB14622AD33E640EE0D87133; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com'), ('Set-Cookie', 'PSTM=1634359528; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com'), ('Set-Cookie', 'BAIDUID=785ED912AB14622A26963A0F829F8297:FG=1; max-age=31536000; expires=Sun, 16-Oct-22 04:45:28 GMT; domain=.baidu.com; path=/; version=1; comment=bd'), ('Set-Cookie', 'BDSVRTM=8; path=/'), ('Set-Cookie', 'BD_HOME=1; path=/'), ('Set-Cookie', 'H_PS_PSSID=34067_31253_34711_34525_34584_34518_34832_34806_34578_26350_34424_34473_34691_34673; path=/; domain=.baidu.com'), ('Strict-Transport-Security', 'max-age=172800'), ('Traceid', '1634359528059018317810073466834918573084'), ('Vary', 'Accept-Encoding'), ('Vary', 'Accept-Encoding'), ('X-Frame-Options', 'sameorigin'), ('X-Ua-Compatible', 'IE=Edge,chrome=1'), ('Connection', 'close'), ('Transfer-Encoding', 'chunked')]
+# 獲取一些狀態訊息
+
+# 一個類型 HTTPresponse
+# 六個方法 read() readline() readlines() getcode() geturl() getheaders()
