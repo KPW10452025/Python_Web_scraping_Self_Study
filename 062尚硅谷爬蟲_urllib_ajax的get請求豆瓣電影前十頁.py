f6b8bb2ce7bf5719a467627e8dd4e5a7fffcd6c0
PoWei <10452025@mail.ntou.edu.tw>
2021-10-17 22:27:01 +0800

    每一頁都有自己的請求物件訂製

--- 062尚硅谷爬蟲_urllib_ajax的get請求豆瓣電影前十頁.py
+++ 062尚硅谷爬蟲_urllib_ajax的get請求豆瓣電影前十頁.py
@@ -13,3 +13,22 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # start  0   20  40  60
 
 # start = (page - 1)*20
+
+# 下載豆瓣電影前十頁數據
+
+# 基本三步驟：
+# 一、請求物件訂製。
+# 二、獲取響應數據。
+# 三、數據下載到本地。
+
+# 思考邏輯：
+# 因為每一頁的 url 都不一樣，所以步驟一要做些改變
+# 而每一頁都要經歷步驟二和步驟三
+
+# 程式的入口
+if __name__ == '__main__':
+    start_page = int(input('請輸入起始的頁碼'))
+    end_page = int(input('請輸入結束的頁碼'))
+
+    for page in range(start_page, end_page+1):
+        # 每一頁都有自己的請求物件訂製。
