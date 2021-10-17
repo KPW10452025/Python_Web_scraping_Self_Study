e80f25eb714990301c90b4eccc3860bfed3f0d1d
PoWei <10452025@mail.ntou.edu.tw>
2021-10-17 09:54:59 +0800

    尋找請求網址 Request URL

--- 059尚硅谷爬蟲_urllib_post請求谷歌翻譯.py
+++ 059尚硅谷爬蟲_urllib_post請求谷歌翻譯.py
@@ -4,3 +4,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # 獲取 百度翻譯 playground 的網頁源碼
+
+# 尋找請求網址 Request URL
+# 須在 Network 中慢慢觀察並尋找
+url = 'https://fanyi.baidu.com/sug'
