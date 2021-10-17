64d6a26259f190ecf20e5507e0e52709cd5a100a
PoWei <10452025@mail.ntou.edu.tw>
2021-10-17 09:59:09 +0800

    尋找 From Data

--- 059尚硅谷爬蟲_urllib_post請求谷歌翻譯.py
+++ 059尚硅谷爬蟲_urllib_post請求谷歌翻譯.py
@@ -8,3 +8,13 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # 尋找請求網址 Request URL
 # 須在 Network 中慢慢觀察並尋找
 url = 'https://fanyi.baidu.com/sug'
+
+# 請求對象訂製
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
+}
+
+# 尋找 From Data
+data = {
+    'kw' : 'playground'
+}
