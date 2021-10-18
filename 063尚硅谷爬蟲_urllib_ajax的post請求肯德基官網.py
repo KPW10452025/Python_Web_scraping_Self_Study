69e34f9b959993ace724dfc7013956a4c821e2f2
PoWei <10452025@mail.ntou.edu.tw>
2021-10-18 18:38:35 +0800

    create_request() 的基本三要素：url, data, headers

--- 063尚硅谷爬蟲_urllib_ajax的post請求肯德基官網.py
+++ 063尚硅谷爬蟲_urllib_ajax的post請求肯德基官網.py
@@ -33,7 +33,23 @@ import urllib.parse
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-base_url = 'http://www.kfc.com.cn/kfccda/ashx/GetStoreList.ashx?op=cname'
+def create_request(page):
+    # create_request() 的基本三要素：url, data, headers
+    base_url = 'http://www.kfc.com.cn/kfccda/ashx/GetStoreList.ashx?op=cname'
+
+    data = {
+        'cname': '北京',
+        'pid': '',
+        'pageIndex': page,
+        'pageSize': 10,
+    }
+
+    # 和 GET 不一樣的是，POST 的 urllib.parse.urlencode(data) 後面要加個 encode("utf-8")
+    data = urllib.parse.urlencode(data).encode("utf-8")
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    }
 
 if __name__ == "__main__":
     start_page = int(input("請輸入起始頁碼："))
@@ -41,4 +57,5 @@ if __name__ == "__main__":
 
     for page in range(start_page, end_page + 1):
         print(page)
-        
+        # 請求物件訂製
+        create_request(page)
