c9b587d9294fe429784d62cdacc79a3eb0e8963c
PoWei <10452025@mail.ntou.edu.tw>
2021-10-19 14:31:45 +0800

    故意亂寫 url 造成異常

--- 064尚硅谷爬蟲_urllib_異常.py
+++ 064尚硅谷爬蟲_urllib_異常.py
@@ -4,7 +4,10 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # 故意寫錯 url 看會造成什麼樣的報錯
-url = 'https://blog.csdn.net/m0_58649824/article/details/1207793561'
+# url = 'https://blog.csdn.net/m0_58649824/article/details/1207793561'
+
+# 這次故意亂寫，隨便創造一個 url
+url = 'http://www.thisisnotaurl7777777.com'
 
 headers = {
     'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
@@ -23,6 +26,8 @@ try:
     print(content)
     # 由於故意修改 url 後導致以下異常
     # urllib.error.HTTPError: HTTP Error 404: Not Found
+    # 亂寫 url 後導致以下異常
+    # urllib.error.URLError
 
 except urllib.error.HTTPError:
     print("系統施工升級中......")
