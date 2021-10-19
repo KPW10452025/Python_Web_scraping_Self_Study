2f8e3d2f81ea3f4134c12c6ae49756f5ccc8bce0
PoWei <10452025@mail.ntou.edu.tw>
2021-10-19 14:19:25 +0800

    運用 try 和 except 進行暫時錯誤排除

--- 064尚硅谷爬蟲_urllib_異常.py
+++ 064尚硅谷爬蟲_urllib_異常.py
@@ -1,4 +1,5 @@
 import urllib.request
+import urllib.error
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -9,15 +10,22 @@ headers = {
     'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
 }
 
-# 請求物件訂製
-request = urllib.request.Request(url=url, headers=headers)
+try:
+    # 請求物件訂製
+    request = urllib.request.Request(url=url, headers=headers)
 
-# 模擬瀏覽器向伺服器發送請求，並獲得響應 response
-response = urllib.request.urlopen(request)
+    # 模擬瀏覽器向伺服器發送請求，並獲得響應 response
+    response = urllib.request.urlopen(request)
 
-# 獲得響應 response 的內容 content
-content = response.read().decode('utf-8')
+    # 獲得響應 response 的內容 content
+    content = response.read().decode('utf-8')
 
-print(content)
-# 由於故意修改 url 後導致以下異常
-# urllib.error.HTTPError: HTTP Error 404: Not Found
+    print(content)
+    # 由於故意修改 url 後導致以下異常
+    # urllib.error.HTTPError: HTTP Error 404: Not Found
+
+except urllib.error.HTTPError:
+    print("系統施工升級中......")
+
+# 因為已經知道出現的錯誤是 urllib.error.HTTPError
+# 故 import urllib.error 後，運用 try 和 except 進行暫時錯誤排除
