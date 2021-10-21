347afaba31e722c396ace045575c765d6188cf7c
PoWei <10452025@mail.ntou.edu.tw>
2021-10-21 19:46:26 +0800

    小修改

--- 067尚硅谷爬蟲_urllib_代理.py
+++ 067尚硅谷爬蟲_urllib_代理.py
@@ -2,7 +2,7 @@ import urllib.request
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url = "http://www.baidu.com/s?wd=ip"
+url = "http://baidu.com/s?wd=ip"
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
@@ -12,7 +12,7 @@ request = urllib.request.Request(url=url, headers=headers)
 
 proxies = {
     # 寫法 "http" : "IP位置:port碼"
-    # "http" : "115.29.170.58:8118"
+    "http" : "181.52.85.249:36107"
 }
 
 handler = urllib.request.ProxyHandler(proxies = proxies)
@@ -32,7 +32,7 @@ response = opener.open(request)
 
 content = response.read().decode("utf-8")
 
-# print(content)
+print(content)
 
-with open("IP_Address.html", mode="w", encoding="utf-8") as fp:
-    fp.write(content)
+# with open("IP_Address.html", mode="w", encoding="utf-8") as fp:
+    # fp.write(content)
