14370e94de811f61eb642d878de520896602a7a9
PoWei <10452025@mail.ntou.edu.tw>
2021-10-22 11:49:12 +0800

    xpath 語法說明："//地址"

--- 070.py
+++ 070.py
@@ -8,7 +8,19 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # (2) 服務器響應的數據 response.read().decode('utf-8')
     # etree.HRML()
 
-# tree01 = etree.parse('/Users/kuopowei/Developer/Python/尚硅谷爬蟲/070_urllib_xpath.html')
-tree01 = etree.parse('070_urllib_xpath.html')
+# xpath 解析本地文件
+tree = etree.parse('070_urllib_xpath.html')
 
-print(tree01)
+# 語法
+# tree01.xpath('//地址')
+# 地址開頭一定要加上 //
+
+# 尋找有幾個 li
+li_list = tree.xpath('//li')
+print("The length of li_list =", len(li_list))
+# The length of li_list = 7
+
+# 尋找有幾個 ul
+li_list = tree.xpath('//ul')
+print("The length of li_list =", len(li_list))
+# The length of li_list = 2
