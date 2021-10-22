29cdd8791d3905a55361236209d19a45fa8b50d0
PoWei <10452025@mail.ntou.edu.tw>
2021-10-22 13:31:54 +0800

    etree.HTML() 解析獲取的源碼

--- 071.py
+++ 071.py
@@ -3,6 +3,7 @@
 # 三、pinrt()
 
 import urllib.request
+from lxml import etree
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -17,3 +18,18 @@ request = urllib.request.Request(url=url, headers=headers)
 response = urllib.request.urlopen(request)
 
 content = response.read().decode("utf-8")
+
+# 解析服務器的問件
+tree = etree.HTML(content)
+
+# 獲取想要的文件
+# tree.xpath() 括號中的路徑，可以配合 chrome 瀏覽器的插件 Xpath helper 尋找
+result = tree.xpath('//input[@id="su"]/@value')
+
+# xpath 的返回值是一個列表類型的數據
+print(result)
+# ['百度一下']
+
+result = tree.xpath('//input[@id="su"]/@value')[0]
+print(result)
+# 百度一下
