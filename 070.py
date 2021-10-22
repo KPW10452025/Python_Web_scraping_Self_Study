6851e4f4d9579b52c877c88a776c6318fe47d5f6
PoWei <10452025@mail.ntou.edu.tw>
2021-10-22 11:23:29 +0800

    Debug：修改檔案名稱

--- 070.py
+++ 070.py
@@ -4,4 +4,11 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # xpath 解析
 # (1) 本地文件
+    # etree.parse()
 # (2) 服務器響應的數據 response.read().decode('utf-8')
+    # etree.HRML()
+
+# tree01 = etree.parse('/Users/kuopowei/Developer/Python/尚硅谷爬蟲/070_urllib_xpath.html')
+tree01 = etree.parse('070_urllib_xpath.html')
+
+print(tree01)
