e507bad58e6d53730b29551b7fa6fc472782887d
PoWei <10452025@mail.ntou.edu.tw>
2021-10-24 17:50:56 +0800

    使用 .find_element_by_name()

--- 079_Selenium.py
+++ 079_Selenium.py
@@ -17,4 +17,17 @@ browser.get(url)
 
 # 【 元素定位 】
 # 檢查瀏覽器原始碼，找到「百度一下」按鈕的位置的 id
-button = browser.find_element_by_id('su')
+# 使用 .find_element_by_id() 根據 id 來找到物件
+button1 = browser.find_element_by_id('su')
+# print(button1)
+# <selenium.webdriver.remote.webelement.WebElement (session="943c3a752fc597456f45ae47d0f8295e", element="e1c9ccb4-91c8-4720-8674-b0c5093cf623")>
+# 出現類似此數據代表有找到物件
+
+# 檢查瀏覽器原始碼，找到「文字輸入欄位」位置的 name
+# 使用 .find_element_by_name() 根據 name 來找到物件
+button2 = browser.find_element_by_name('wd')
+# print(button2)
+# <selenium.webdriver.remote.webelement.WebElement (session="59febc09bd0a108575fbe59c6028d59f", element="cd95faaf-1f20-43e5-9d3e-b79fe832933f")>
+# 出現類似此數據代表有找到物件
+
+browser.close()
