5f8b35896c7ca23ca44b82da12a55abefa4caa62
PoWei <10452025@mail.ntou.edu.tw>
2021-10-25 16:49:24 +0800

    使用 .get_attribute('class')

---
+++ 080_Selenium.py
@@ -0,0 +1,20 @@
+from os import path
+from selenium import webdriver
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+path = '/Users/kuopowei/Developer/Python/尚硅谷爬蟲/chromedriver'
+
+browser = webdriver.Chrome(path)
+
+url = 'http://www.baidu.com'
+
+browser.get(url)
+
+# 【 元素定位 】
+# 檢查瀏覽器原始碼，找到「百度一下」按鈕的位置的 id
+# 使用 .find_element_by_id() 根據 id 來找到物件
+input = browser.find_element_by_id('su')
+
+# 使用 .get_attribute('class') 返回查詢 class 的屬性值
+print(input.get_attribute('class'))
