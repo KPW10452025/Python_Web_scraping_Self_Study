29e5102920d6b9a3391045b6db02f310ba02d502
PoWei <10452025@mail.ntou.edu.tw>
2021-10-25 18:07:20 +0800

    編寫各種交互

--- 081_Selenium.py
+++ 081_Selenium.py
@@ -11,4 +11,29 @@ url = 'https://www.baidu.com'
 
 browser.get(url)
 
-browser.close()
+import time
+time.sleep(2) # 讓程式停止運作 2 秒
+
+# 獲取「收尋輸入匡」的對象
+input = browser.find_element_by_id('kw')
+
+# 在「收尋輸入匡」中輸入「周杰倫」
+input.send_keys('周杰倫')
+
+time.sleep(2)
+
+# 獲取「百度一下」的按鈕
+button = browser.find_element_by_id('su')
+
+# 點擊「百度一下」的按鈕
+button.click()
+
+time.sleep(2)
+
+# 滑動頁面到底部
+js_bottom = 'document.documentElement.scrollTop=100000'
+browser.execute_script(js_bottom)
+
+time.sleep(2)
+
+# browser.close()
