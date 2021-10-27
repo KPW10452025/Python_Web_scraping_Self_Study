38a8b575287aecec162603dec10acd19284af3c4
PoWei <10452025@mail.ntou.edu.tw>
2021-10-27 09:24:56 +0800

    簡單進度回顧

---
+++ 085_requests_get.py
@@ -0,0 +1,17 @@
+# urllib
+# 一、一個類型與六個請求
+# 二、get 請求
+# 三、post 請求
+# 四、ajax 的 get 請求
+# 五、ajax 的 post 請求
+# 六、cookie 登入
+# 七、代理
+
+# requests
+# 一、一個類型與六個屬性
+# 二、get 請求
+# 三、post 請求
+# 四、代理
+# 五、cookie 驗證碼
+
+
