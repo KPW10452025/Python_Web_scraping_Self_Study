732d2017a892288fec767983ae3977908cd49450
PoWei <10452025@mail.ntou.edu.tw>
2021-10-27 12:33:41 +0800

    requests get 請求的基本架構與小結論

--- 085_requests_get.py
+++ 085_requests_get.py
@@ -14,4 +14,27 @@
 # 四、代理
 # 五、cookie 驗證碼
 
+import requests
 
+url = 'https://www.baidu.com/s?'
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+}
+
+data = {
+    'wd' : '北京'
+}
+
+# url 請求資源路徑
+# params 參數
+# kwargs 字典
+response = requests.get(url=url, params=data, headers=headers)
+
+content = response.text
+
+print(content)
+
+# 參數使用 params 傳遞
+# 參數無需 urlencode 編碼
+# 不需要請求物件訂製
