c00835f2bbe64998242e530fa48e9d110d724666
PoWei <10452025@mail.ntou.edu.tw>
2021-10-27 14:46:44 +0800

    使用代理

--- 087_requests_proxy.py
+++ 087_requests_proxy.py
@@ -1,6 +1,9 @@
 import requests
 
-url = 'https://www.baidu.com/s?'
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context
+
+url = 'http://www.baidu.com/s?'
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
@@ -10,9 +13,16 @@ data = {
     'wd' : 'ip'
 }
 
-response = requests.get(url=url,params=data,headers=headers)
+proxy = {
+    'http://' : '211.65.197.93:80'
+}
+
+response = requests.get(url=url,params=data,headers=headers,proxies=proxy)
+# response = requests.get(url=url,params=data,headers=headers)
 
 content = response.text
 
 with open('daili.html', mode='w', encoding='utf-8') as fp:
     fp.write(content)
+
+# 搜尋關鍵字「本机IP」
