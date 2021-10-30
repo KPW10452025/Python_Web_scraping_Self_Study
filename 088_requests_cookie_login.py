3a6293559466988387b0b0e7b06c0ea046c59df3
PoWei <10452025@mail.ntou.edu.tw>
2021-10-30 11:25:35 +0800

    獲得 content 並確認無反爬

---
+++ 088_requests_cookie_login.py
@@ -0,0 +1,41 @@
+# 古詩文網
+# https://so.gushiwen.cn/user/collect.aspx
+
+# 尋找「登入接口」
+# 藉由錯誤密碼做登入，在 Network 找到名為 login.aspx... 的位置
+
+# From Data
+# __VIEWSTATE: yeepjl7ZFflIVse6iz3Til11o7Ks05mKYYAg2zMWrUr9iZOAdGlHrIi3ysjpBIDEbLEWaz3LI8gnbg7cXbOO/hTfQAshm36k0PeWbXTqgXWpa7G2YAw/N6Bn2Jc=
+# __VIEWSTATEGENERATOR: C93BE1AE
+# from: http://so.gushiwen.cn/user/collect.aspx
+# email: kuopowei@gmail.com
+# pwd: powei4gushiwe
+# code: 66vw
+# denglu: 登录
+
+# 觀察後得知
+# __VIEWSTATE, __VIEWSTATEGENERATOR, code 這三個是變量
+
+# 難點
+# 一、__VIEWSTATE, __VIEWSTATEGENERATOR
+# 二、驗證碼 code
+
+# 一般情況下，看不的數據（__VIEWSTATE, __VIEWSTATEGENERATOR）都是在頁面源碼中
+# 藉由觀看網頁原始碼，搜尋 __VIEWSTATE 後發現，__VIEWSTATE, __VIEWSTATEGENERATOR 數據都屬於 type="hidden"
+# type="hidden" 隱藏域
+
+import requests
+
+# 登入頁面的 url 地址
+url = 'https://so.gushiwen.cn/user/login.aspx?from=http://so.gushiwen.cn/user/collect.aspx'
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+}
+
+# 獲取頁面的源碼
+response = requests.get(url=url, headers=headers)
+content = response.text
+# print(content)
+# 對 content 做搜尋以確認無反爬
+# 成功搜尋到 __VIEWSTATE 
