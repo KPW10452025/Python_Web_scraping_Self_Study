42ba7f9d8b67df2f6962bac11fe0a75000db472d
PoWei <10452025@mail.ntou.edu.tw>
2021-10-31 12:01:05 +0800

    創建 Read_Me 文件並依步驟創建爬蟲檔案

---
+++ scrapy_baidu_091/scrapy_baidu_091/spiders/baidu.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['http://www.baidu.com']
+    start_urls = ['http://http://www.baidu.com/']
+
+    def parse(self, response):
+        pass
