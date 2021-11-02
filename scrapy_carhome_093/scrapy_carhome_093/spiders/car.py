e1d5069c9d41031bba29c4e75d779b5a4c208b33
PoWei <10452025@mail.ntou.edu.tw>
2021-11-02 09:15:10 +0800

    發現錯誤：url 問題

--- scrapy_carhome_093/scrapy_carhome_093/spiders/car.py
+++ scrapy_carhome_093/scrapy_carhome_093/spiders/car.py
@@ -4,7 +4,9 @@ import scrapy
 class CarSpider(scrapy.Spider):
     name = 'car'
     allowed_domains = ['https://car.autohome.com.cn/price/brand-15.html']
-    start_urls = ['https://car.autohome.com.cn/price/brand-15.html/']
+    # 當 url 後綴是 html 時，不能加 /
+    start_urls = ['https://car.autohome.com.cn/price/brand-15.html']
 
     def parse(self, response):
         print("***************this is for testing***************")
+        # ***************this is for testing*************** 爬蟲成功
