0e4c8abb6b9849fea5087020fbeba7d7bf67d8f5
PoWei <10452025@mail.ntou.edu.tw>
2021-11-02 09:25:02 +0800

    成功獲取寶馬全車系名稱

--- scrapy_carhome_093/scrapy_carhome_093/spiders/car.py
+++ scrapy_carhome_093/scrapy_carhome_093/spiders/car.py
@@ -8,5 +8,46 @@ class CarSpider(scrapy.Spider):
     start_urls = ['https://car.autohome.com.cn/price/brand-15.html']
 
     def parse(self, response):
-        print("***************this is for testing***************")
+
+        # print("***************this is for testing***************")
         # ***************this is for testing*************** 爬蟲成功
+
+        name_list = response.xpath('//div[@class="main-title"]/a/text()')
+        # print(name_list)
+        # Selector 的 list
+        # [<Selector xpath='//div[@class="main-title"]/a/text()' data='宝马1系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马3系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系新能源'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X1'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X1新能源'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X2'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马iX3'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X3'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马2系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马4系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系(进口)'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马6系GT'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马7系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马8系'>]
+        
+        for name in name_list:
+            # print(name)
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马1系'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马3系'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系新能源'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X1'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X1新能源'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X2'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马iX3'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X3'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马2系'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马4系'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系(进口)'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马6系GT'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马7系'>
+            # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马8系'>
+
+            print(name.extract())
+            # 宝马1系
+            # 宝马3系
+            # 宝马5系
+            # 宝马5系新能源
+            # 宝马X1
+            # 宝马X1新能源
+            # 宝马X2
+            # 宝马iX3
+            # 宝马X3
+            # 宝马2系
+            # 宝马4系
+            # 宝马5系(进口)
+            # 宝马6系GT
+            # 宝马7系
+            # 宝马8系
