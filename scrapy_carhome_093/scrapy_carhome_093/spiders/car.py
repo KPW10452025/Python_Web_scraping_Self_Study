8f5c8b1e0265ec5d480a8e65bd444a8cffb00fbc
PoWei <10452025@mail.ntou.edu.tw>
2021-11-02 09:33:18 +0800

    結合車系與價格

--- scrapy_carhome_093/scrapy_carhome_093/spiders/car.py
+++ scrapy_carhome_093/scrapy_carhome_093/spiders/car.py
@@ -17,7 +17,7 @@ class CarSpider(scrapy.Spider):
         # Selector 的 list
         # [<Selector xpath='//div[@class="main-title"]/a/text()' data='宝马1系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马3系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系新能源'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X1'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X1新能源'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X2'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马iX3'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马X3'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马2系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马4系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马5系(进口)'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马6系GT'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马7系'>, <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马8系'>]
         
-        for name in name_list:
+        # for name in name_list:
             # print(name)
             # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马1系'>
             # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马3系'>
@@ -35,7 +35,7 @@ class CarSpider(scrapy.Spider):
             # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马7系'>
             # <Selector xpath='//div[@class="main-title"]/a/text()' data='宝马8系'>
 
-            print(name.extract())
+            # print(name.extract())
             # 宝马1系
             # 宝马3系
             # 宝马5系
@@ -53,8 +53,8 @@ class CarSpider(scrapy.Spider):
             # 宝马8系
         
         price_list = response.xpath('//span[@class="font-arial"]/text()')
-        for price in price_list:
-            print(price.extract())
+        # for price in price_list:
+        #     print(price.extract())
             # 20.58-24.68万
             # 29.39-39.89万
             # 42.39-55.19万
@@ -70,3 +70,24 @@ class CarSpider(scrapy.Spider):
             # 58.39-68.39万
             # 82.80-261.20万
             # 96.20-119.80万
+        
+        for i in range(len(name_list)):
+            name = name_list[i].extract()
+            price = price_list[i].extract()
+            print(name, price)
+            # 宝马1系 20.58-24.68万
+            # 宝马3系 29.39-39.89万
+            # 宝马5系 42.39-55.19万
+            # 宝马5系新能源 49.99-53.69万
+            # 宝马X1 27.98-33.98万
+            # 宝马X1新能源 39.98万
+            # 宝马X2 28.58-32.38万
+            # 宝马iX3 39.99-43.99万
+            # 宝马X3 39.28-47.98万
+            # 宝马2系 26.38-33.98万
+            # 宝马4系 36.38-55.88万
+            # 宝马5系(进口) 42.39-60.39万
+            # 宝马6系GT 58.39-68.39万
+            # 宝马7系 82.80-261.20万
+            # 宝马8系 96.20-119.80万
+            
