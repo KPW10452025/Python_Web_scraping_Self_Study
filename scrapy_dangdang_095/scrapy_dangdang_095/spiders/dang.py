79da2c615c60b7e9948f3ba8609e3e4fe7b71653
PoWei <10452025@mail.ntou.edu.tw>
2021-11-03 15:23:23 +0800

    確認無 robots 協議

--- scrapy_dangdang_095/scrapy_dangdang_095/spiders/dang.py
+++ scrapy_dangdang_095/scrapy_dangdang_095/spiders/dang.py
@@ -4,7 +4,9 @@ import scrapy
 class DangSpider(scrapy.Spider):
     name = 'dang'
     allowed_domains = ['http://category.dangdang.com/cp01.01.02.00.00.00.html']
-    start_urls = ['http://http://category.dangdang.com/cp01.01.02.00.00.00.html/']
+    start_urls = ['http://category.dangdang.com/cp01.01.02.00.00.00.html']
 
     def parse(self, response):
-        pass
+        print("******************************")
+        print("******************************")
+        print("******************************")
