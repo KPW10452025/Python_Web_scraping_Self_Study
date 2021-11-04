f2767a76b3fccfaa5993ebc37f0af994f5552451
PoWei <10452025@mail.ntou.edu.tw>
2021-11-04 14:00:00 +0800

    所有 selector 對象，都可以再次調用 xpath 方法

--- scrapy_dangdang_095/scrapy_dangdang_095/items.py
+++ scrapy_dangdang_095/scrapy_dangdang_095/items.py
@@ -9,4 +9,11 @@ import scrapy
 class ScrapyDangdang095Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # 通俗的說，就是要下載的數據有什麼
+    
+    # 圖片
+    src = scrapy.Field()
+    # 名字
+    name = scrapy.Field()
+    # 價格
+    price = scrapy.Field()
--- scrapy_dangdang_095/scrapy_dangdang_095/spiders/dang.py
+++ scrapy_dangdang_095/scrapy_dangdang_095/spiders/dang.py
@@ -1,5 +1,6 @@
 import scrapy
 
+# http://category.dangdang.com/cp01.01.02.00.00.00.html
 
 class DangSpider(scrapy.Spider):
     name = 'dang'
@@ -7,6 +8,16 @@ class DangSpider(scrapy.Spider):
     start_urls = ['http://category.dangdang.com/cp01.01.02.00.00.00.html']
 
     def parse(self, response):
-        print("******************************")
-        print("******************************")
-        print("******************************")
+        # pipeline 下載數據
+        # items 定義數據結構的
+        # src = //ul[@id="component_59"]/li/a/img/@src
+        # alt = //ul[@id="component_59"]/li/a/img/@alt
+        # price = //ul[@id="component_59"]/li/p[@class="price"]/span[1]/text()
+
+        # 經過觀察發現 src, alt, price 三個目標數據的在 li 下面
+
+        # 所有 selector 對象，都可以再次調用 xpath 方法
+        li_list = response.xpath('//ul[@id="component_59"]/li')
+        
+        for li in li_list:
+            print(li)
