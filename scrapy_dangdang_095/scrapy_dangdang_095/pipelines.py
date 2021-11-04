6c665c1c234f9beb2839e9046f7ec01610f17b5e
PoWei <10452025@mail.ntou.edu.tw>
2021-11-04 20:26:28 +0800

    使用 with open 寫入資料的問題

--- scrapy_dangdang_095/scrapy_dangdang_095/pipelines.py
+++ scrapy_dangdang_095/scrapy_dangdang_095/pipelines.py
@@ -11,9 +11,12 @@ from itemadapter import ItemAdapter
 class ScrapyDangdang095Pipeline:
     # item 就是 yield 後面的 book 對象
     def process_item(self, item, spider):
+        
+        # 企業級開發中不推薦 with open 方法，因為每傳遞過來一條文件，就要開啟一次文件，對文件的開關過於頻繁，容易出問題
+        
         # 一、write 發法，寫入值必須是字符串
         # 二、write 模式，每次都會打開文件，覆蓋之前內容，所以下載下來的文件只有最後一本書的資料
-        with open('book.json', mode='a', encoding='utf-8')as fp:
-            fp.write(str(item))
+        # with open('book.json', mode='a', encoding='utf-8')as fp:
+        #     fp.write(str(item))
 
         return item
