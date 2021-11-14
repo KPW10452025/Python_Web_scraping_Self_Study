5170083ac7658c89bdf1ebe1c814484a71427a50
PoWei <10452025@mail.ntou.edu.tw>
2021-11-14 12:54:13 +0800

    修改 allowed_domains 後成功執行爬蟲

--- scrapy_movie_099/scrapy_movie_099/spiders/mv.py
+++ scrapy_movie_099/scrapy_movie_099/spiders/mv.py
@@ -3,7 +3,8 @@ import scrapy
 
 class MvSpider(scrapy.Spider):
     name = 'mv'
-    allowed_domains = ['https://www.dydytt.net/html/gndy/dyzz/index.html']
+    # allowed_domains = ['https://www.dydytt.net/html/gndy/dyzz/index.html']
+    allowed_domains = ['www.dydytt.net']
     start_urls = ['https://www.dydytt.net/html/gndy/dyzz/index.html']
 
     def parse(self, response):
@@ -29,3 +30,6 @@ class MvSpider(scrapy.Spider):
 
     def parse_second(self, response):
         print('========123456123456========')
+        # 實行爬蟲後報錯
+        # 因為目前的 allowed_domains = ['https://www.dydytt.net/html/gndy/dyzz/index.html'] 不包含 url = 'https://www.dydytt.net' + href
+        # 所以須將 allowed_domains 做修改，將允許網域改大一點：allowed_domains = ['www.dydytt.net']
